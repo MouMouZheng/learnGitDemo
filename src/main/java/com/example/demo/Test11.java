91fb7b8626f02d544a2d7e3e1a70ba60a21a0024
zheng <zhengyf@si-tech.com.cn>
2019-04-09 18:36:44 +0800

    123

--- src/main/java/com/example/demo/Test11.java
+++ src/main/java/com/example/demo/Test11.java
@@ -4,7 +4,7 @@ public class Test {
     String aa = "";
     String bb ="bb";
     String cc = "cc";
-    String dd = "dd33123";
+    String dd = "dd33";
 
     public static void main(String[] args) {
         System.out.println("===========");
