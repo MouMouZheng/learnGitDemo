bdfd46ad27e591c8e4b727511ced03afa218666d
zheng <zhengyf@si-tech.com.cn>
2019-04-10 09:56:36 +0800

    master0702

--- src/main/java/com/example/demo/Test11.java
+++ src/main/java/com/example/demo/Test11.java
@@ -3,7 +3,7 @@ package com.example.demo;
 public class Test {
     String aa = "34";
     String bb ="bb456bb7777";
-    String cc = "cc";
+    String cc = "cc222";
     String dd = "dd33";
 
     public static void main(String[] args) {
