81ea2da059abc0520042bf218ae49cefcc1b97b2
zheng <zhengyf@si-tech.com.cn>
2019-04-10 13:56:24 +0800

    master0706

--- src/main/java/com/example/demo/Test1.java
+++ src/main/java/com/example/demo/Test1.java
@@ -3,7 +3,7 @@ package com.example.demo;
 public class Test {
     String bb ="bb";
     String cc = "cc123123";
-    String dd = "dd12312";
-    String ee = "123123";
-    String ff = "ddddfffff";
+    String dd = "dd12312qwe";
+    String ee = "123123eqw";
+    String ff = "ddddfffffqwe";
 }
