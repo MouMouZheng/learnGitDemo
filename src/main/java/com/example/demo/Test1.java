110cb35b64ffd88708729cfeac7c652861920129
zheng <zhengyf@si-tech.com.cn>
2019-04-10 11:20:23 +0800

    master0606

--- src/main/java/com/example/demo/Test1.java
+++ src/main/java/com/example/demo/Test1.java
@@ -1,8 +1,8 @@
 package com.example.demo;
 
 public class Test {
-    String bb ="bb1231";
-    String cc = "cc123";
+    String bb ="bb";
+    String cc = "cc";
     String dd = "dd12312";
     String ee = "123123";
     String ff = "ddddfffff";
